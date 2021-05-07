06e42e92671b370f6af0fac19689db82efb49f4a
Lizângelo Araujo <lizangelo.cskd@gmail.com>
2021-05-07 15:33:44 -0400

    calculadoraoop

---
+++ CalculadoraCSharp-master/CalculadoraEncapsuladaOOP.ConsoleApp/Calculadora.cs
@@ -0,0 +1,222 @@
+﻿using System.Linq;
+using System;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace Calculadora.ConsoleApp
+{   
+    public class Calculadora
+    {
+        public string primeiroValor;
+        public string segundoValor;
+        public string operador;
+        public string[] calculosRealizados = new string[100];
+
+        public bool TentaCalcular(string expressao, out double resultado, out string mensagem)
+        {
+            double primeiroNumero, segundoNumero;
+            mensagem = " ";
+            resultado = 0.0;
+
+            if (!VerificaExpressao(expressao))
+            {
+                mensagem = " Entrada inválida, por favor insira uma entrada válida ";
+                return false;
+            }
+
+            
+            // VALIDAÇÃO
+            if (double.TryParse(primeiroValor, out primeiroNumero) == false)
+            {
+                mensagem = "Primeiro número inválido";
+                return false;
+            }
+            if (double.TryParse(segundoValor, out segundoNumero) == false)
+            {
+                mensagem = "Segundo número inválido";
+                return false;
+            }
+            if (operador != "+" && operador != "-" && operador != "*" && operador != "/")
+            {
+                mensagem = "Operador inválido";
+                return false;
+            }
+            if (operador == "/" && segundoNumero == 0)
+            {
+                mensagem = "Não é possível dividir por zero";
+                return false;
+            }
+
+            if (operador == "+")
+                resultado = primeiroNumero + segundoNumero;
+
+            else if (operador == "-")
+                resultado = primeiroNumero - segundoNumero;
+
+            else if (operador == "*")
+                resultado = primeiroNumero * segundoNumero;
+
+            else if (operador == "/")
+                resultado = primeiroNumero / segundoNumero;
+
+
+            calculosRealizados[ObterPosicaoVazia()] = $" {primeiroNumero}  {operador}  {segundoNumero}  = {resultado.ToString("F3", CultureInfo.InvariantCulture)} ";
+
+            return true;
+        }
+
+        // Verifica operadores
+
+        public bool VerificaExpressao(string expressao)
+        {
+
+            if (VerificarSimboloAdicao(expressao))
+            {
+                if ((VerificarSimboloSubtracao(expressao)) || (VerificarSimboloMultiplicacao(expressao)) ||
+                   (VerificarSimboloDivisao(expressao)))
+                {
+                    return false;
+                }
+                if (Regex.Matches(expressao, "\\+").Count > 1)
+                {
+                    return false;
+                }
+                primeiroValor = expressao.Split('+')[0];
+                segundoValor = expressao.Split('+')[1];
+                operador = "+";
+                return true;
+            }
+
+            if (VerificarSimboloSubtracao(expressao))
+            {
+                if ((VerificarSimboloAdicao(expressao)) || (VerificarSimboloMultiplicacao(expressao)) ||
+                   (VerificarSimboloDivisao(expressao)))
+                {
+                    return false;
+                }
+                if (Regex.Matches(expressao, "-").Count > 1)
+                {
+                    return false;
+                }
+                primeiroValor = expressao.Split('-')[0];
+                segundoValor = expressao.Split('-')[1];
+                operador = "-";
+                return true;
+            }
+
+            if (VerificarSimboloMultiplicacao(expressao))
+            {
+                if ((VerificarSimboloSubtracao(expressao)) || (VerificarSimboloAdicao(expressao)) ||
+                   (VerificarSimboloDivisao(expressao)))
+                {
+                    return false;
+                }
+                if (Regex.Matches(expressao, "\\*").Count > 1)
+                {
+                    return false;
+                }
+                primeiroValor = expressao.Split('*')[0];
+                segundoValor = expressao.Split('*')[1];
+                operador = "*";
+                return true;
+            }
+
+            if (VerificarSimboloDivisao(expressao))
+            {
+                if ((VerificarSimboloSubtracao(expressao)) || (VerificarSimboloMultiplicacao(expressao)) ||
+                    (VerificarSimboloAdicao(expressao)))
+                {
+                    return false;
+                }
+                if (Regex.Matches(expressao, "/").Count > 1)
+                {
+                    return false;
+                }
+                primeiroValor = expressao.Split('/')[0];
+                segundoValor = expressao.Split('/')[1];
+                operador = "/";
+                return true;
+            }
+            return false;
+            
+        }
+
+        public bool VerificarSimboloSubtracao(string expressao)
+        {
+            if (expressao.Contains("-"))
+            {
+                return true;           
+            }
+            return false;
+        }
+
+        public bool VerificarSimboloMultiplicacao(string expressao)
+        {
+            if (expressao.Contains("*"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool VerificarSimboloDivisao(string expressao)
+        {
+            if (expressao.Contains("/"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool VerificarSimboloAdicao(string expressao)
+        {
+            if (expressao.Contains("+"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int ObterPosicaoVazia()
+        {
+            int posicaoVazia = 0;
+
+            for (int i = 0; i < calculosRealizados.Length; i++)
+            {
+                if (calculosRealizados[i] == null)
+                {
+                    posicaoVazia = i; break;
+                }
+            }
+            return posicaoVazia;
+        }
+
+        public int QuantidadeCalculosRealizados()
+        {
+            int qtdCalculos = 0;
+
+            for (int i = 0; i < calculosRealizados.Length; i++)
+            {
+                if (calculosRealizados[i] != null)
+                {
+                    qtdCalculos++;
+                }
+            }
+            return qtdCalculos;
+        }
+
+        public string[] CalculosRealizados()
+        {
+            int qtd = QuantidadeCalculosRealizados();
+
+            string[] calculosRealizadosAux = new string[qtd];
+
+            for (int i = 0; i < qtd; i++)
+            {
+                calculosRealizadosAux[i] = calculosRealizados[i];
+            }
+
+            return calculosRealizadosAux;
+        }
+    }
+}
---
+++ CalculadoraCSharp-master/CalculadoraEncapsuladaOOP.ConsoleApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Calculadora.ConsoleApp
+{
+    class ProgramaOrientadoObjetos
+    {
+        
+        static void Main(string[] args)
+        {
+           
+            Calculadora minhaCalculadora = new Calculadora();
+
+            while (true)
+            {
+                string opcao = Menu();
+
+                if (opcao.Equals("sair", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                else if (opcao == "1")
+                    RealizarCalculos(minhaCalculadora);
+
+                else if (opcao == "2")
+                    VisualizarHistoricoCalculos(minhaCalculadora);
+                                
+                Console.Clear();
+            }
+        }
+
+        private static void VisualizarHistoricoCalculos(Calculadora minhaCalculadora)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Visualizando o histórico de calculos...\n");
+
+            string[] calculosRealizados = minhaCalculadora.CalculosRealizados();
+
+            foreach (string calculoRealizado in calculosRealizados)
+                Console.WriteLine(calculoRealizado);
+
+            Console.ReadLine();
+        }
+
+        private static void RealizarCalculos(Calculadora minhaCalculadora)
+        {
+            double resultado;
+            string mensagem;
+            bool conseguiuCalcular;
+            string expressao;
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Digite a expressão conforme o exemplo → 123 + 12");
+                Console.Write("Expressão: ");
+                expressao = Console.ReadLine();
+
+                conseguiuCalcular = minhaCalculadora.TentaCalcular(expressao, out resultado, out mensagem);
+
+                if (conseguiuCalcular == false)
+                {
+                    Console.WriteLine(mensagem);
+                    Console.ReadLine();
+                }
+
+            } while (conseguiuCalcular == false);
+
+            Console.WriteLine($" O resultado é: {resultado}");
+
+            Console.ReadLine();
+        }
+
+        private static string Menu()
+        {
+            Console.WriteLine(" ========== x CALCULADORA x ==========\n");
+
+            Console.WriteLine("   Escolha a opção desejada abaixo ");
+
+            Console.WriteLine("   # → 1 - para realizar o cálculo ");            
+
+            Console.WriteLine("  # → SAIR - para encerrar o programa ");
+
+            string opcao = Console.ReadLine();
+
+            return opcao;
+        }
+    }
+}
